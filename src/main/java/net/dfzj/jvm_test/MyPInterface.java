9de621cd0c210b0026a909b8a91ef8f35790f5dc
qiankeqin <qiankeqin@51xf.cn>
2020-08-27 10:45:08 +0800

    jvm

---
+++ src/main/java/net/dfzj/jvm_test/MyPInterface.java
@@ -0,0 +1,13 @@
+package net.dfzj.jvm_test;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @author qiankeqin
+ * @Description: DESCRIPTION
+ * @date 2020-08-25 09:07
+ */
+public interface MyPInterface {
+    public static int a = 1;
+
+}
